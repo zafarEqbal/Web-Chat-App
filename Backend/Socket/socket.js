49260bbcf4e511ab49255ebb5725f0f2492b2e06
himanshu321jsr <2022ugee040@nitjsr.ac.in>
2025-07-13 04:09:52 +0530

    Update socket.js

--- Backend/Socket/socket.js
+++ Backend/Socket/socket.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ['https://web-chat-app-frontend-zu95.onrender.com'],
+    origin: ['https://web-chat-app-frontend-1o2g.onrender.com'],
     methods: ["GET", "POST"],
   },
 });
